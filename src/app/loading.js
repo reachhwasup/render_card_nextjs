dbe1620992f85d76b91be258e543af169beb544f
reachhwasup <paoponareach08@gmail.com>
2023-05-17 13:44:47 +0700

    render api to card

---
+++ src/app/loading.js
@@ -0,0 +1,10 @@
+export default function Loading(){
+
+    return(
+        <>
+        <h1>Loading-----</h1>
+        <img src="https://media.tenor.com/zAJyO4ukfvAAAAAd/skeleton-skeletons.gif" alt="" />
+    </>
+        )
+    
+}
